f9c4ed6751a2310054ce419095182be7c0dc4b88
bvelez <Bienvenido.Velez@upr.edu>
2017-02-07 06:38:16 -0400

    Added main java class

---
+++ Lab3/src/Lab3Exercises.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Lab3Exercises {
+	public static void main(String[] args) {
+		JFrame myFrame = new JFrame("Hello, world!");
+		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(200, 200);
+		myFrame.setSize(400, 100);
+		myFrame.setVisible(true);
+	}
+}
