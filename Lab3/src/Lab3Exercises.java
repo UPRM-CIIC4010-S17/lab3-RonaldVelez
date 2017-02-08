bc88f761ff023d82bbc0504305ff1188c785cfb1
4015b17 <4015b17@tamarindo.ece.uprm.edu>
2017-02-08 10:40:58 -0400

    Changed name, size and centered Jframe

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Ronald J Velez");
-		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(920, 400);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
