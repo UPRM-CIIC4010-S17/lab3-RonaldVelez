ec608f0f66fec196f8fa5b836a01a006882c4d3f
4015b17 <4015b17@tamarindo.ece.uprm.edu>
2017-02-08 10:27:56 -0400

    Changed name and centered Jframe

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,10 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("Ronald J Velez");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(920, 400);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
